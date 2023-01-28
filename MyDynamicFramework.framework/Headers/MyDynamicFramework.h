36d24f66c615606fdff27c80b2999bd3d9b0196e
龚浩植 <675560219@qq.com>
2023-01-28 17:33:17 +0800

    dqwd

---
+++ MyDynamicFramework.framework/Headers/MyDynamicFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyDynamicFramework.h
+//  MyDynamicFramework
+//
+//  Created by 龚浩植 on 2023/1/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyDynamicFramework.
+FOUNDATION_EXPORT double MyDynamicFrameworkVersionNumber;
+
+//! Project version string for MyDynamicFramework.
+FOUNDATION_EXPORT const unsigned char MyDynamicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyDynamicFramework/PublicHeader.h>
+
+
+#import "MyDynamicViewController.h"
