8f7dbe97ca2b846ad047ed15689a533a23ad6aab
龚浩植 <675560219@qq.com>
2023-01-29 18:01:58 +0800

    ces

---
+++ HotFramework.framework/Headers/HotFramework.h
@@ -0,0 +1,19 @@
+//
+//  HotFramework.h
+//  HotFramework
+//
+//  Created by 龚浩植 on 2023/1/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HotFramework.
+FOUNDATION_EXPORT double HotFrameworkVersionNumber;
+
+//! Project version string for HotFramework.
+FOUNDATION_EXPORT const unsigned char HotFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HotFramework/PublicHeader.h>
+
+#import "MainHomeViewController.h"
+
---
+++ HotFramework.framework/Headers/MainHomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainHomeViewController.h
+//  HotFramework
+//
+//  Created by 龚浩植 on 2023/1/29.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainHomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
